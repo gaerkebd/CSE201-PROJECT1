d05c3291bbb481b4144ad931b4fe5deba2b09f5f
RobertMacy19 <macyrf@miamioh.edu>
2025-04-04 14:13:46 -0400

    Add files via upload

---
+++ MoneyCheck.java
@@ -0,0 +1,14 @@
+
+public class MoneyCheck {
+
+	public static void main(String[] args) {
+		int money = 0;
+		if (money == 0) {
+			System.out.println("You ended with no money! You lost!");
+			
+		} else {
+			System.out.println("You ended with $" + money);
+		}
+	}
+
+}
